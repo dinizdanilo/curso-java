cec52843c603f03cffa6596a2ca22cb8d234f43f
Danilo Diniz <dgdevbr@gmail.com>
2026-02-04 10:25:08 -0300

    primeiro exercício de soma

---
+++ exercicios/src/ExercicioSoma.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ExercicioSoma {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        int x = sc.nextInt();
+
+        System.out.print("Digite o segundo número: ");
+        int b = sc.nextInt();
+
+        System.out.printf("Você digitou %d e %d%n", x, b);
+
+        int soma = (x + b);
+
+        System.out.printf("%nA soma desses dois números é igual a: " + soma);
+    }
+}
