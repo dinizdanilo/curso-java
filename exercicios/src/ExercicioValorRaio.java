be5a5111d843f58b4bbc0c0861f8983b9074dc8a
Danilo Diniz <dgdevbr@gmail.com>
2026-02-04 10:35:36 -0300

    exercicio calculo da area do circulo

---
+++ exercicios/src/ExercicioValorRaio.java
@@ -0,0 +1,18 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioValorRaio {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o valor do raio do círculo: ");
+        double x = sc.nextDouble();
+
+        double pi = 3.14159;
+        double area = pi * (x * x);
+        System.out.printf("A área do círculo é igual a %.4f", area);
+    }
+}
