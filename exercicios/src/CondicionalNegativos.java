1943088a96bef07d96ac27feb1cfa0117f18e758
Danilo Diniz <dgdevbr@gmail.com>
2026-02-09 08:31:55 -0300

    exercicio condicional para verificação de números negativos ou positivos

--- exercicios/src/CondicionalNegativos.java
+++ exercicios/src/CondicionalNegativos.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class CondicionalNegativos {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+
+        if (sc.hasNextInt()) {
+            int numero = sc.nextInt();
+                if (numero < 0) {
+                    System.out.println("O número digitado é negativo.");
+                }
+                else {
+                    System.out.println("O número digitado é positivo.");
+                }
+        }
+        else {
+            System.out.println("Digite apenas números inteiros!");
+        }
+        sc.close();
+    }
 }
