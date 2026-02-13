889f4663a7ed7c0261ae742df0e33d071216999e
Danilo Diniz <dgdevbr@gmail.com>
2026-02-13 08:38:28 -0300

    fatorial de um número utilizando estrutura de repetição for

--- exercicios/src/Fatorial.java
+++ exercicios/src/Fatorial.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Fatorial {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um valor N: ");
+        int N = sc.nextInt();
+
+        long fatorial = 1;
+        if (N == 0) {
+            System.out.println("O fatorial de " + N + " é: " + fatorial);
+        }
+        else {
+            for (int i = 1; i <= N; i++) {
+                fatorial *= i;
+                System.out.println("O fatorial de " + N + " é: " + fatorial);
+
+            }
+        }
+    }
 }
