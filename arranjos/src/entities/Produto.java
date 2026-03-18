f99a7896fdbe02786ee2bf7a72c542f587acb768
Danilo Diniz <dgdevbr@gmail.com>
2026-03-18 08:49:29 -0300

    criando classe de produto para aplicar operações

---
+++ arranjos/src/entities/Produto.java
@@ -0,0 +1,22 @@
+package entities;
+
+public class Produto {
+
+    private String name;
+    private double price;
+
+    public String getName(){
+        return name;
+    }
+    public void setName() {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
